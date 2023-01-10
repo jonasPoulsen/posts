bcb093eed26149abb9057d5ebe8e8d026060cf23
jonasPoulsen <jonasppoulsen@gmail.com>
2023-01-10 13:51:45 +0100

    Testing the branch tree

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+//Header.jsx
+//import React, { useState } from 'react';
+//import '../styles/Header.css';
+
+function Header() {
+    return(
+        <div className='Header'>Welcome to the "Posts App" - by Testhub.tech</div>
+    );
+};
+
+export default Header;
